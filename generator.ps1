21e71aceb2bd25f8ed3c905505987ec96a2b8844
J-Nyarangi <113386253+J-Nyarangi@users.noreply.github.com>
2023-09-30 04:24:15 +0300

    Create generator.ps1
    generator file

---
+++ generator.ps1
@@ -0,0 +1,17 @@
+# Define the Python script you want to run (main.py in this case)
+$pythonScript = "main.py"
+
+# Define the function you want to execute (e.g., "generate_excel_files", "separate_files", or "train_translations")
+$functionName = $args[0]
+
+# Run the Python script using the python command with the chosen function as an argument
+python $pythonScript $functionName
+
+python $pythonScript
+
+#./generator.ps1 generate_excel_files
+#./generator.ps1 separate_files
+#./generator.ps1 train_translations
+#./generator.ps1
+
+
